2fc01ba9295ed4ec2d37f06198c4aa136c4beba3
ssf <2351149669@qq.com>
2021-06-23 00:06:23 +0800

    day02

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.gmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://manager.gmall.com");
+        config.addAllowedOrigin("http://localhost:1000");
+        config.addAllowedOrigin("http://127.0.0.1:1000");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(configurationSource);
+    }
+}
